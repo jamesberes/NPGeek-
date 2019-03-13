799172793e5cad5197df074436b83c5621b605b5
James Beres <jamesberes90@gmail.com>
2019-03-13 10:20:21 -0400

    Added DAL skeleton files and dependency injection!

---
+++ 13-Capstone/Capstone.Web/DAL/WeatherSqlDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Web.DAL;
+
+namespace Capstone.Web.DAL
+{
+    public class WeatherSqlDAL : IWeatherDAL
+    {
+        private string connectionString;
+
+        public WeatherSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
