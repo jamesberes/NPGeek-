e12582eb2d815b9e7f1c38a1540e0a49f41d5bc2
James Beres <jamesberes90@gmail.com>
2019-03-13 11:55:09 -0400

    added methods to surveySqlDAL and changed weatherSqlDAL forecast name for partly cloudy to partlyCloudy to match image name

--- 13-Capstone/Capstone.Web/DAL/ISurveyDAL.cs
+++ 13-Capstone/Capstone.Web/DAL/ISurveyDAL.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.Web.Models;
 
 namespace Capstone.Web.DAL
 {
     public interface ISurveyDAL
     {
+        Dictionary<string, int> GetFavoriteParkBySurveyCount();
+        void SaveNewSurvey(Survey survey);
     }
 }
---
+++ 13-Capstone/Capstone.Web/Models/Survey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Survey
+    {
+        public int SurveyId { get; set; }
+        public string ParkName { get; set; }
+        public string ParkCode { get; set; }
+        public string Email { get; set; }
+        public string State { get; set; }
+        public string ActivityLevel { get; set; }
+    }
+}
