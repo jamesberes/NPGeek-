ebb34b1e9e6c8cc3f0df50f8682d519f759917bc
Mackenzie Jones <mackenzie.jones15@aol.com>
2019-03-13 11:18:24 -0400

    Add DetailViewModel

---
+++ 13-Capstone/Capstone.Web/Models/DetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class DetailViewModel
+    {
+        public Park Park { get; set; }
+        public Weather Weather { get; set; }
+    }
+}
