8f1216bcc4004e88b7efcdd28c0965d84f29167c
James Beres <jamesberes90@gmail.com>
2019-03-13 16:02:20 -0400

    Added styling to detail page and added Recommendation Function to DetailViewModel for displaying temperature recommendations

--- 13-Capstone/Capstone.Web/Models/DetailViewModel.cs
+++ 13-Capstone/Capstone.Web/Models/DetailViewModel.cs
@@ -15,5 +15,38 @@ namespace Capstone.Web.Models
             Park = park;
             Weather = weather;
         }
+
+        private Dictionary<string, string> WeatherRecommendations = new Dictionary<string, string>()
+        {
+            { "snow", "Pack your snowshoes! " },
+            { "rain", "Pack your rain gear and waterproof shoes! " },
+            { "thunderstorms", "Seek shelter and avoid hiking on exposed ridges. " },
+            { "sunny", "Don't forget the sunblock! " },
+            { "cloudy", " " },
+            { "partlyCloudy", " " }
+        };
+
+        public string TempRecommendations(Weather weather)
+        {
+            string tempRecommend = "";
+            if (weather.High > 75)
+            {
+                tempRecommend = "Bring an extra gallon of water!";
+            }
+            else if (weather.High - weather.Low > 20)
+            {
+                tempRecommend = "Wear breathable layers!";
+            }
+            else if (weather.Low < 20)
+            {
+                tempRecommend = "Danger! Frigid temperatures!";
+            }
+            else
+            {
+                tempRecommend = "";
+            }
+
+            return WeatherRecommendations[weather.Forecast] + tempRecommend;
+        }
     }
 }
